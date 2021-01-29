0a106e1b9bdde5cddab7131bfc4f3902f6fb1984
fullstack82 <fullstack8210@gmail.com>
2021-01-30 00:05:19 +0100

    fechas y matematicas

--- predefinidas.php
+++ predefinidas.php
@@ -8,4 +8,16 @@ var_dump($nombre);     // var_dump saca todos los datos de nombre en este caso
 echo "<hr/>";
 
 // Fechas 
-echo date('d-m-y');
+$time = time();
+echo date("d-m-y (H:i:s)", $time); // Fecha y hora actual;
+echo "<br/>";
+echo time();
+
+echo "<hr/>";
+
+// Matematicas
+echo "Raiz cuadrada de 10: ".sqrt(10);  // (sqtr) saca la raiz cuadrada;
+
+echo "<hr/>";
+
+echo "Numero aleatorio entre 10 y 40: ".rand(10, 40); // (rand) nos muestra un numero aleatorio;
