a88dada131d98ec3c7c172c951c6c02384bf38a3
fullstack82 <fullstack8210@gmail.com>
2021-01-29 22:20:31 +0100

    explicacion variables locales y globales

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+* Variables locales: Son las que se definen dentro de una función y no pueden ser
+* usadas fuera de la función, solo estan disponibles dentro, a no ser que haga un return.
+*
+* Variables globales: son las que se declaran fuera de una función y estan disponibles 
+* dentro y fuera de las funciones;
+*/
