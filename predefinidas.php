8f643ae0c69cfaf6883d599c704b3ed80d8b7e24
fullstack82 <fullstack8210@gmail.com>
2021-01-30 00:18:57 +0100

    funciones generales

--- predefinidas.php
+++ predefinidas.php
@@ -21,3 +21,18 @@ echo "Raiz cuadrada de 10: ".sqrt(10);  // (sqtr) saca la raiz cuadrada;
 echo "<hr/>";
 
 echo "Numero aleatorio entre 10 y 40: ".rand(10, 40); // (rand) nos muestra un numero aleatorio;
+
+echo "<hr/>";
+
+echo "Número pi:".pi(); // (pi) nos muestra el número pi;
+
+echo "<br/>";
+
+echo "Redondear: ".round(7.89234, 1); // Numero a redondear, y decimales que quieres que se vean son los parametros de round();
+
+
+echo "<hr/>";
+// Más funciones generales
+
+echo gettype($nombre);  // Devuelve el tipo de la variable;
+
